64ed9ad3ef4f69c5553cd46cbdc096a4b5150006
Rahul Bora <rahulbora101@gmail.com>
2026-04-18 22:41:58 +0530

    refactor: clean up unused font imports and update dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import '@fontsource-variable/geist'
-import '@fontsource/caveat/400.css'
-import '@fontsource/caveat/700.css'
 import './index.css'
 import App from './App.tsx'
 
